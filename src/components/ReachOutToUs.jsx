5d36801093d7091a576c5be997c66b6235057263
drex007 <vykeoj@gmail.com>
2025-11-11 13:04:50 +0100

    feat: reachout sub text alignment

--- src/components/ReachOutToUs.jsx
+++ src/components/ReachOutToUs.jsx
@@ -7,7 +7,7 @@ const ReachOutToUs = () => {
             <div className='relative min-h-[200px] bg-gray-100 rounded-3xl lg:flex lg:justify-between items-center'>
                 <div className='lg:w-4/5 p-8'>
                     <p className='font-poppins lg:text-2xl text-xl my-4 lg:text-left text-center '>Reach Out To Us</p>
-                    <p className='font-spacegrotesk lg:w-2/5 w-full lg:text-[15px] text-[12px]'>Contact us today to learn more about how our platform and also for support</p>
+                    <p className='font-spacegrotesk lg:w-2/5 w-full lg:text-[15px] text-[12px] lg:text-left text-center'>Contact us today to learn more about how our platform and also for support</p>
             
                 </div>
                     <div className='lg:w-1/5 w-full px-4'>
