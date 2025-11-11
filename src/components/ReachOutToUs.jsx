c21541f63431c39a423ab0ab0089e05999300093
drex007 <vykeoj@gmail.com>
2025-11-11 13:02:56 +0100

    feat: reachout text alignment

--- src/components/ReachOutToUs.jsx
+++ src/components/ReachOutToUs.jsx
@@ -6,7 +6,7 @@ const ReachOutToUs = () => {
         <div className=' lg:px-16 px-4 lg:mt-40  lg:mb-24  mb-4'>
             <div className='relative min-h-[200px] bg-gray-100 rounded-3xl lg:flex lg:justify-between items-center'>
                 <div className='lg:w-4/5 p-8'>
-                    <p className='font-poppins lg:text-2xl text-xl my-4 '>Reach Out To Us</p>
+                    <p className='font-poppins lg:text-2xl text-xl my-4 lg:text-left text-center '>Reach Out To Us</p>
                     <p className='font-spacegrotesk lg:w-2/5 w-full lg:text-[15px] text-[12px]'>Contact us today to learn more about how our platform and also for support</p>
             
                 </div>
